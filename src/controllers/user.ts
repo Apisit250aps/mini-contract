389c63fdebaf3ed2774bb5a8c59d013129dad7b5
YUapisit <aps.apisit250@gmail.com>
2026-02-03 07:03:22 +0700

    feat: add user update functionality and corresponding API route

--- src/controllers/user.ts
+++ src/controllers/user.ts
@@ -1,6 +1,11 @@
 import { safeValidate } from '@/lib/utils'
 import { BaseUser, User } from '@/models/entities/user'
-import { createUser, getUserByName } from '@/models/repositories/user'
+import {
+  createUser,
+  getUserById,
+  getUserByName,
+  updateUser,
+} from '@/models/repositories/user'
 import { NextRequest, NextResponse } from 'next/server'
 
 async function CreateUser(
@@ -57,4 +62,60 @@ async function CreateUser(
   }
 }
 
-export { CreateUser }
+async function UpdateUser(
+  req: NextRequest,
+  { params }: { params: Promise<{ id: string }> },
+): Promise<NextResponse<ApiResponse<User>>> {
+  try {
+    const data = await req.json()
+    const { id } = await params
+    //
+    const user = await getUserById(id)
+    if (!user) {
+      return NextResponse.json(
+        {
+          success: false,
+          message: 'User not found',
+          error: 'Not Found',
+        },
+        { status: 404 },
+      )
+    }
+    const validation = await safeValidate(
+      BaseUser.omit({
+        id: true,
+        lastLogin: true,
+        createdAt: true,
+        updatedAt: true,
+      }).partial(),
+      data,
+    )
+    if (validation.error || !validation.data) {
+      return NextResponse.json(
+        {
+          success: false,
+          message: 'Validation Error',
+          error: validation.error,
+        },
+        { status: 400 },
+      )
+    }
+    const updated = await updateUser(id, validation.data)
+    return NextResponse.json({
+      success: true,
+      message: 'User updated successfully',
+      data: updated,
+    })
+  } catch (error) {
+    return NextResponse.json(
+      {
+        success: false,
+        message: 'Internal Server Error',
+        error: (error as Error).message,
+      },
+      { status: 500 },
+    )
+  }
+}
+
+export { CreateUser, UpdateUser }
